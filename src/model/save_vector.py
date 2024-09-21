7a3fca2b6e9b4e8f734a08a67af63e70fce3aefd
junhaa <2171326@hansung.ac.kr>
2024-09-21 23:56:39 +0900

    :sparkles: FEAT. 벡터 인덱스 번호 return 변경

--- src/model/save_vector.py
+++ src/model/save_vector.py
@@ -3,12 +3,13 @@ import faiss
 # word2vec 모델 dimension 수
 dimensions = 200
 
+# L2 거리 기반의 인덱스 생성
+index = faiss.IndexFlatL2(dimensions)
+
 def save_vector(avg_vector):
     # 2차원 배열로 변환
     avg_vector= avg_vector.reshape(1, -1)
 
-    # L2 거리 기반의 인덱스 생성
-    index = faiss.IndexFlatL2(dimensions)
-    print(avg_vector)
-
     index.add(avg_vector)
+
+    return index.ntotal
