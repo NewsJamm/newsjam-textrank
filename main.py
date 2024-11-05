93b5f1dbb771cd304163c8ab1989ea8dd0688fe7
junhaa <2171326@hansung.ac.kr>
2024-11-06 02:21:25 +0900

    :art: CHORE. Optional None 처리 추가

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class NewsImageCrawlingRequest(BaseModel):
     news_url: str
 
 class NewsImageCrawlingResponse(BaseModel):
-    image_url: Optional[str]
+    image_url: Optional[str] = None
 
 # 예시 기사
 title = "한국 경제 위기설, 진실은?"
